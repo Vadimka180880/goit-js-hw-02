8a75f1043296f0ca2e8a7638316e340544b29978
Vadimka180880 <vadim.te180880@gmail.com>
2024-10-13 22:04:28 -0400

    Test

---
+++ task-1.js
@@ -0,0 +1,9 @@
+function calculateTotalPrice (orderedQuantity, pricePerItem) {
+return  orderedQuantity * pricePerItem;
+}
+
+console.log(calculateTotalPrice(5, 100)); 
+console.log(calculateTotalPrice(8, 60));    
+console.log(calculateTotalPrice(3, 400));  
+console.log(calculateTotalPrice(1, 3500)); 
+console.log(calculateTotalPrice(12, 70));
